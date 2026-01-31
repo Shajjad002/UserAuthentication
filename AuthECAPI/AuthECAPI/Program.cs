05efc023c70e7128022c2764064f8a0068232f0a
Shajjad002 <shajjd002@gmail.com>
2026-01-31 19:31:42 +0600

    Programe file optimize

--- AuthECAPI/AuthECAPI/Program.cs
+++ AuthECAPI/AuthECAPI/Program.cs
@@ -1,3 +1,4 @@
+using AuthECAPI.Controllers;
 using AuthECAPI.Extensions;
 using AuthECAPI.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -15,13 +16,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-
 builder.Services.InjectDbContext(builder.Configuration)
                 .AddIdentityHandlersAndStores()
                 .ConfigureIdentityOptions()
                 .AddIdentityAuth(builder.Configuration)
                 .AddSwaggerExplorer();
 
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.ConfigureSwaggerExplorer()
+    .AddIdentityAuthMiddlewares(); 
+
+#region Config. CORS
 
 builder.Services.AddCors(options =>
 {
@@ -34,25 +41,11 @@ builder.Services.AddCors(options =>
         });
 });
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-#region Config. CORS
-
 app.UseCors("AllowAngular");
 
 #endregion
 
 app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.MapControllers();
 
@@ -60,91 +53,7 @@ app
     .MapGroup("/api")
     .MapIdentityApi<AppUser>(); // No need to specify user type again here
 
-app.MapPost("/api/signup", async (UserManager<AppUser> userManager, [FromBody] UserRegistrationModel userRegistrationModel) =>
-{
-    AppUser newUser = new AppUser
-    {
-        UserName = userRegistrationModel.Email,
-        Email = userRegistrationModel.Email,
-        FullName = userRegistrationModel.FullName,// You can set this based on your requirements
-    };
-    var result = await userManager.CreateAsync(newUser, userRegistrationModel.Password);
-    if (result.Succeeded)
-    {
-        // return Results.Ok(result);
-        return Results.Ok(new
-        {
-            succeeded = true,
-            message = "User registered successfully"
-        });
-    }
-    else
-    {
-        //return Results.BadRequest(result);
-        return Results.BadRequest(new
-        {
-            succeeded = false,
-            errors = result.Errors
-        });
-
-    }
-
-});
-
-app.MapPost("/api/signin", async (UserManager<AppUser> userManager, [FromBody] LoginModel loginModel) =>
-{
-    var user = await userManager.FindByEmailAsync(loginModel.Email);
-    if (user != null)
-    {
-        var isPasswordValid = await userManager.CheckPasswordAsync(user, loginModel.Password);
-        if (isPasswordValid)
-        {
-            var signInKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["AppSettings:JWTSecret"]!));
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
-                    new Claim("UserID",user.Id.ToString())
-                }),
-                Expires = DateTime.UtcNow.AddMinutes(15),
-                SigningCredentials = new SigningCredentials(signInKey, SecurityAlgorithms.HmacSha256Signature)
-            };
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
-            var token = tokenHandler.WriteToken(securityToken);
-            return Results.Ok(new { token });
-        }
-        else
-        {
-            return Results.BadRequest(new
-            {
-                succeeded = false,
-                message = "password is incorrect."
-            });
-        }
-    }
-    else
-    {
-        return Results.BadRequest(new
-        {
-            succeeded = false,
-            message = "Username or password is incorrect."
-        });
-    }
-
-});
+app.MapIdentityUserEndpoints();
 
 app.Run();
 
-public class UserRegistrationModel
-{
-    public string FullName { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-}
-
-public class LoginModel
-{
-    public string Email { get; set; }
-    public string Password { get; set; }
-}
